90831f3e2e49397553639f2267e4e17d2127733b
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2020-06-16 00:39:09 -0400

    added junit for RecipeServiceImpl

---
+++ src/test/java/ed/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package ed.springframework.spring5recipeapp.services;
+
+import ed.springframework.spring5recipeapp.model.Recipe;
+import ed.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this); //this telling mockito give me a mock RecipeRepository
+        recipeService = new RecipeServiceImpl(recipeRepository); // init create recipeservice
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+        assertEquals(recipeSet.size(), 1);
+        verify(recipeRepository, times(1)).findAll(); // we wanna verify that findAll was called once
+    }
+}
